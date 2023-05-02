fab27153b500b610f4a0f19ebb559afbe2b0ab1e
neocitylab <83133442+neocitylab@users.noreply.github.com>
2023-05-02 01:28:58 -0500

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $(".btn-link").click(function(){
+        alert("El articulo fue agregado a su lista de favoritos");
+    })
+
+    $(".btn-primary").click(function(){
+        alert("Plataforma de compra");
+    })
+})
